36c383a75d48ee4f180f79cda0f1f0d11f48eb82
tisha mali <tishamali@gmail.com>
2026-01-07 12:48:59 +0530

    Add grade calculator program in C

---
+++ grade-calculator.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int num;
+    char grade;
+    printf("enter your score:");
+    scanf("%d", &num);
+    // step 1 ternary operator
+    grade=(num >= 90)? 'A':
+          (num >= 80)? 'B':
+          (num >= 70)? 'C':
+          (num >= 60)? 'D':
+          (num >= 50)? 'E':'F';
+    printf("your grade is %c.",grade);
+
+    // step 2 switch case
+
+    switch(grade){
+            case 'A':
+            printf("Excellent work!");
+            break;
+        
+            case 'B':
+            printf("Well done.");
+            break;
+            
+            case 'C':
+            printf("Good job.");
+            break;
+
+            case 'D':
+            printf("you passed,but you cloud do better");
+            break;
+            
+            case 'E':
+            printf("you are passed with grazing marks");
+            break;
+            
+            case 'F':
+            printf("sorry you failed ");
+            break;
+    }
+
+    // step 3 if-else-statement
+    
+    if(grade == 'F'){
+            printf(" Please try again next time.");
+        }
+        else{
+            printf("You are eligible for the next level.");
+            }
+        return 0;
+    
+}
